2b38b2d58971e2d4f1747ae6e3ebba5ff1d6ed17
Gulbara Satybekova <gulbara.satybekova.on@gmail.com>
2022-03-01 21:43:40 +0600

    add mobile style

---
+++ script.js
@@ -0,0 +1,7 @@
+let menuBtn = document.querySelector(".menu-btn");
+
+let menu = document.querySelector(".header-menu");
+
+menuBtn.addEventListener("click", function(){
+    menuBtn.classList.toggle("active");
+})
